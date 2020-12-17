57fd5fefca8a9382d38de73cd7dde531e94be70d
evgeny <evgenykostyukov97@mail.ru>
2020-12-17 19:58:17 +0300

    Epam

---
+++ src/Basics/linearPrograms.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class linearPrograms {
+    public static void main(String[] args) {
+
+        //Task 1   Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c.
+        int a = 2, b = 3, c = 4, z;
+        System.out.println(z = ((a - 3) * b / 2) + c);
+
+        //Task 2   Вычислить значение выражения по формуле (
+        // все переменные принимают действительные значения)
+        double a2 = 2.5, b2 = 3.5, c2 = 4.5, result;
+        double n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+        n1 = Math.pow(b2, 2);
+        n2 = 4 * a2 * c2;
+        n3 = n1 + n2;
+        n4 = Math.sqrt(n3);
+        n5 = b2 + n4;
+        n6 = 2 * a2;
+        n7 = n5 / n6;
+        n8 = Math.pow(a2, 3);
+        n9 = n8 * c2;
+        n10 = Math.pow(b2, -2);
+        result = n7 - n9 + n10;
+        System.out.println("Result function: " + result);
+
+        //Task3 Вычислить значение выражения
+        // по формуле (все переменные принимают действительные значения):
+
+
+    }
+}
