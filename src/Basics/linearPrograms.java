77de6eeaa65317d92926d7cf29ef61cb35f51d5a
evgeny <evgenykostyukov97@mail.ru>
2020-12-18 10:02:57 +0300

    1

--- src/Basics/linearPrograms.java
+++ src/Basics/linearPrograms.java
@@ -1,15 +1,16 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class linearPrograms {
     public static void main(String[] args) {
 
-        //Task 1   Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c.
-        int a = 2, b = 3, c = 4, z;
-        System.out.println(z = ((a - 3) * b / 2) + c);
+        //Task 1   Найдите  значение функции
+        int a = 2, b = 3, c = 4;
+        System.out.println(((a - 3) * b / 2) + c);
 
-        //Task 2   Вычислить значение выражения по формуле (
-        // все переменные принимают действительные значения)
-        double a2 = 2.5, b2 = 3.5, c2 = 4.5, result;
+        //Task 2   Вычислить значение выражения по формуле
+        double a2 = 2.5, b2 = 3.5, c2 = 4.5;
         double n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
         n1 = Math.pow(b2, 2);
         n2 = 4 * a2 * c2;
@@ -21,12 +22,46 @@ public class linearPrograms {
         n8 = Math.pow(a2, 3);
         n9 = n8 * c2;
         n10 = Math.pow(b2, -2);
-        result = n7 - n9 + n10;
-        System.out.println("Result function: " + result);
+        System.out.println("Result function: " + (n7 - n9 + n10));
 
-        //Task3 Вычислить значение выражения
-        // по формуле (все переменные принимают действительные значения):
+        //Task3 Вычислить значение выражения по формуле
+        double x = 3.5, y = 4.5;
+        double q1, q2, q3, q4, q5, q6, q7, q8, q9;
+        q1 = Math.sin(x);
+        q2 = Math.cos(y);
+        q3 = q1 + q2;
+        q4 = Math.cos(x);
+        q5 = Math.sin(y);
+        q6 = q4 - q5;
+        q7 = q3 / q6;
+        q8 = x * y;
+        q9 = Math.tan(q8);
+        System.out.println("Result function: " + (q7 * q9));
 
+        //Task4 Дано действительное число R
+        double R = 987.456;
+        double result = (R * 1000) % 1000 + (int) R / 1000.0;
+        System.out.println(result);
 
+        //Task5 Дано натуральное число Т
+        int T = 7330;
+        int HH, MM, SS;
+        System.out.println("Всего секунд: " + T);
+        HH = T / 3600;
+        System.out.println(HH + " часа(ов)");
+        MM = (T % 3600) / 60;
+        System.out.println(MM + " минут(ы)");
+        SS = T % 60;
+        System.out.println(SS + " секунд(ы)");
+
+        //Task6 Для данной области составить линейную программу
+        Scanner scanner = new Scanner(System.in);
+        int xx, yy;
+        System.out.println("Введите координаты точки: х и у :  ");
+        xx = scanner.nextInt();
+        yy = scanner.nextInt();
+        if (yy < 4 && yy > -3 || xx < 4 && xx > -4) {
+            System.out.println("true");
+        } else System.out.println("false");
     }
 }
